a8f4c655f501b5c2766df791c090e4b39cafa45c
kellynge1ywa <120782457+kellynge1ywa@users.noreply.github.com>
2023-10-03 11:35:35 +0300

    Guessing game

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret_word = "Apple";
+            string guess = "";
+            int guess_limit = 3;
+            int guess_count = 0;
+            bool out_of_guesses = false;
+
+
+            while(guess != secret_word && !out_of_guesses)
+            {
+                if(guess_count < guess_limit)
+                {
+                    Console.Write("enter your guess: ");
+                    guess = Console.ReadLine();
+                    guess_count++;
+
+                }
+                else
+                {
+                    out_of_guesses = true;
+                }
+                
+            }
+            if (out_of_guesses)
+            {
+                Console.WriteLine("You've run out of guesses!!!");
+            }
+            else
+            {
+                Console.WriteLine("CONGRATULATIONS!!!");
+
+
+            }
+
+            //do
+            //{
+            //    Console.Write("enter your guess: ");
+            //    guess = Console.ReadLine();
+
+            //} while (guess != secret_word);
+
+            //Console.WriteLine("you've won!!!");
+
+
+
+        }
+    }
+}
